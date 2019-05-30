ef523c99eb349ffd1bc8e079255d297446ce0d4d
lds <85176878@qq.com>
2019-05-30 15:50:05 +0800

    Update urls.py

--- demo_app/demo/urls.py
+++ demo_app/demo/urls.py
@@ -3,19 +3,22 @@
 from django.urls import include, path
 from django.contrib import admin
 
-# 取消注释接下来的两行以启用管理员：
-import xadmin
-xadmin.autodiscover()
-
-# version模块自动注册需要版本控制的 Model
-from xadmin.plugins import xversion
-xversion.register_models()
-
-
 urlpatterns = [
     path(r'admin', admin.site.urls),
 ]
 
+# ------------------------------------------------------------------------
+# 下面的代码启用 xadmin
+import xadmin
+
+xadmin.autodiscover()
+
+# version模块自动注册需要版本控制的 Model
+from xadmin.plugins import xversion
+
+xversion.register_models()
+
 urlpatterns += [
     path(r'', xadmin.site.urls),
 ]
+# ------------------------------------------------------------------------
